e757d8c5b36dbe5e68cd0e1a6c7131fff79de2b1
Stéphane <stephane.26@hotmail.fr>
2019-11-26 15:55:33 +0100

    update of creation of database

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ puts 'Clean database...'
 User.destroy_all
 
 puts 'Creating 25 fake user...'
-25.times do
+15.times do
   user = User.new(
     username: Faker::Internet.username,
     email: Faker::Internet.email,
@@ -20,16 +20,29 @@ puts 'Creating 25 fake user...'
   )
   user.save!
 
-  seedpackage = Seedpackage.new(
+  3.times do
+    seedpackage = Seedpackage.new(
     name:    Faker::Company.name,
     description: Faker::Lorem.sentence,
     location: Faker::Address.full_address,
     photo: Faker::Internet.url,
     package_price: Faker::Number.between(from: 1, to: 10),
     user: user,
-  )
-  seedpackage.save!
+    )
+    seedpackage.save!
+  end
+
 end
+
+10.times do
+  user = User.new(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: 'password',
+  )
+  user.save!
+end
+
 puts 'Finished!'
 
 puts "user created : #{User.count} "
