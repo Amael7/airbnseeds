58a18c4d0b6840daab613a07a7d477c8346cccd9
Stéphane <stephane.26@hotmail.fr>
2019-11-26 12:41:29 +0100

    add username column to user & create transaction model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :seedpackages
+  has_many :seedpackages, dependent: :destroy
   has_many :transactions
 end
