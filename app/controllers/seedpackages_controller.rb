9de5ff50eab58780512b0756d4d199f17bfe676e
Stéphane <stephane.26@hotmail.fr>
2019-11-27 12:57:05 +0100

    feature is done

--- app/controllers/seedpackages_controller.rb
+++ app/controllers/seedpackages_controller.rb
@@ -1,10 +1,12 @@
 class SeedpackagesController < ApplicationController
+  before_action :set_seedpackages, only: %i[show destroy]
+
   def index
     @seedpackages = Seedpackage.all
   end
 
   def show
-    @seedpackage = Seedpackage.find(params[:id])
+    # @seedpackage = Seedpackage.find(params[:id])
     @transaction = Transaction.new
   end
 
@@ -22,7 +24,17 @@ class SeedpackagesController < ApplicationController
     end
   end
 
+  def destroy
+    # @seedpackage = Seedpackage.find(params[:id])
+    @seedpackage.destroy
+    redirect_to seedpackages_path(@seedpackage)
+  end
+
   private
+
+  def set_seedpackages
+    @seedpackage = Seedpackage.find(params[:id])
+  end
 
   def seedpackage_params
     params.require(:seedpackage).permit(:name, :description, :location, :photo, :package_price)
